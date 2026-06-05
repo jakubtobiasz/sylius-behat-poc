7a4e2c0dea7642731325e35f636d49ff41c017e0
Jacob Tobiasz <jakub.tobiasz@icloud.com>
2026-06-05 14:12:01 +0200

    Remove Playwright-related classes and tests from the Sylius Behat package, including PlaywrightSessionContext, PlaywrightDriverFactory, SyliusBehatExtension, and associated test files. This cleanup eliminates unused code and dependencies.

--- src/Behat/SyliusBehatExtension.php
+++ src/Behat/SyliusBehatExtension.php
@@ -13,8 +13,6 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 final class SyliusBehatExtension implements Extension
 {
-    private bool $minkExtensionFound = false;
-
     public function getConfigKey(): string
     {
         return 'alphpaca_sylius_behat';
@@ -28,23 +26,14 @@ final class SyliusBehatExtension implements Extension
         }
 
         $minkExtension->registerDriverFactory(new PlaywrightDriverFactory());
-        $this->minkExtensionFound = true;
     }
 
     public function configure(ArrayNodeDefinition $builder): void
     {
-        $builder
-            ->addDefaultsIfNotSet()
-            ->children()
-                ->scalarNode('javascript_session')->defaultValue('playwright')->end()
-            ->end()
-        ;
     }
 
     public function load(ContainerBuilder $container, array $config): void
     {
-        $container->setParameter('alphpaca_sylius_behat.javascript_session', $config['javascript_session']);
-        $container->setParameter('alphpaca_sylius_behat.mink_extension_found', $this->minkExtensionFound);
     }
 
     public function process(ContainerBuilder $container): void
